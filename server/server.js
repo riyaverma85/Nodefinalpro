81c585eede7d9e5294fa633747ed681129f0aef9
riyaverma85 <riyaverma2976@gmail.com>
2025-09-11 23:19:37 -0700

    index

--- server/server.js
+++ server/server.js
@@ -2,9 +2,22 @@
 const express=require("express");
 const cors=require("cors");
 require("dotenv").config();
-
+const mongoose=require("mongoose");
+const doctorRoute=require("./routes/doctorRoute");
+const bodyparser=require("body-parser");
 app.use(cors())
 const app=express();
+
+//Database connection
+mongoose.connect(process.env.DBCON).then(()=>{
+    console.log("Database connected");
+})
+//body parser middleware
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+
+
+app.use("/doctor",doctorRoute);
 const Port=process.env.PORT || 8000
 app.listen(Port,()=>{
     console.log(`Server is running on port ${Port}`);
