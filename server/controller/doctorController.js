494d5976c35ffabf0af139a2c945244cec113a3e
riyaverma85 <riyaverma2976@gmail.com>
2025-09-11 23:02:45 -0700

    register

---
+++ server/controller/doctorController.js
@@ -0,0 +1,5 @@
+
+const doctorModel=require("../models/doctorModel");
+const regisTration=async(req,res)=>{
+
+}
--- server/routes/doctorRoute.js
+++ server/routes/doctorRoute.js
@@ -2,5 +2,7 @@
 const express=require("express");
 const route=express.Router();
 const uploadMiddleware=require("../middlewares/cloudinary");
+const doctorController=require("../controller/doctorController");
 
+route.post("/registration",uploadMiddleware.upload.single("file"),doctorController.regisTration)
 module.exports=route;
