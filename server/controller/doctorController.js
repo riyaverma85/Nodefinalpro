d77d20c141cd3356c04094f9bb81b90d35c86426
riyaverma85 <riyaverma2976@gmail.com>
2025-09-11 23:14:27 -0700

    agsh

--- server/controller/doctorController.js
+++ server/controller/doctorController.js
@@ -1,5 +1,27 @@
 
 const doctorModel=require("../models/doctorModel");
 const regisTration=async(req,res)=>{
+       try {
+        const{doctorname,speciality,city,address,contact,email,password}=req.body;
+        const Doctor=await doctorModel.create({
+            doctorname:doctorname,
+            speciality:speciality,
+            city:city,
+            address:address,
+            image:req.file.path,
+            contact:contact,
+            email:email,
+            password:password
+        })
+        res.json({
+            success:true,
+            fileUrl:req.file.path      //Cloudinary file
+        })
 
+       } catch (error) {
+         res.status(500).json({success:false,message: erroe.message})
+       }
+}
+module.exports={
+    regisTration
 }
