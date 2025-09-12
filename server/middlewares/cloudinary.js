a9545bb7d453c2bf164ffaa97e43729e863b3cb8
riyaverma85 <riyaverma2976@gmail.com>
2025-09-11 22:58:39 -0700

    route

--- server/middlewares/cloudinary.js
+++ server/middlewares/cloudinary.js
@@ -3,8 +3,23 @@ const multer=require("multer");
 const {v2}=require("cloudinary");
 const {CloudinaryStorage}=require("multer-storage-cloudinary");
 
+//cloudinary configuration
 v2.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
     api_secret:process.env.CLOUDINARY_API_SECRET
 })
+
+//Multer storage with cloudinary
+const storage=new CloudinaryStorage({
+    cloudinary:v2,
+    params:{
+        folder:"mern_uploads",   //folder name on cloudinary
+        allowed_formats:["jpg","png","jpeg","webp","pdf"]    //allowed file  types
+    },
+})
+
+const upload=multer({storage:storage});
+module.exports={
+    upload
+}
---
+++ server/routes/doctorRoute.js
@@ -0,0 +1,6 @@
+
+const express=require("express");
+const route=express.Router();
+const uploadMiddleware=require("../middlewares/cloudinary");
+
+module.exports=route;
