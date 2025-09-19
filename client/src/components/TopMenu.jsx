16bf9d3eb1d953fbf902846792e41a6cae1597e4
riyaverma85 <riyaverma2976@gmail.com>
2025-09-19 10:58:00 -0700

    nvacss

--- client/src/components/TopMenu.jsx
+++ client/src/components/TopMenu.jsx
@@ -5,23 +5,29 @@ import {Link} from "react-router-dom"
 const TopMenu=()=>{
     return(
         <>
-        <Navbar  id="topmenu">
-        <Container>
-          <Navbar.Brand href="#home" id='head'>DOCTRO</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="home" className='pages'>Home</Nav.Link>
-            <Nav.Link as={Link} to="about" className='pages'>About</Nav.Link>
-            <Nav.Link as={Link} to="search" className='pages'>Search</Nav.Link>
-            <Nav.Link as={Link} to="getappoint" className='pages'>Getappointment</Nav.Link>
-            <Nav.Link as={Link} to="contact" className='pages'>Contact</Nav.Link>
-            
-            {/* <Nav.Link as={Link} to="searchname" className='pages'>SearchByName</Nav.Link>
+        <Navbar expand="md" id="topmenu" variant="dark">
+  <Container>
+    <Navbar.Brand href="#home" id='head'>DOCTRO</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link as={Link} to="/home" className='pages'>Home</Nav.Link>
+        <Nav.Link as={Link} to="/about" className='pages'>About</Nav.Link>
+        <Nav.Link as={Link} to="/search" className='pages'>Search</Nav.Link>
+        <Nav.Link as={Link} to="/getappoint" className='pages'>Getappointment</Nav.Link>
+        <Nav.Link as={Link} to="/contact" className='pages'>Contact</Nav.Link>
+        {/* <Nav.Link as={Link} to="searchname" className='pages'>SearchByName</Nav.Link>
             <Nav.Link as={Link} to="searchcity" className='pages'>SearchByCity</Nav.Link>
             <Nav.Link as={Link} to="speciality" className='pages'>SearchBySpeciality</Nav.Link> */}
             
-          </Nav>
-        </Container>
-      </Navbar>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+            
+            
+          
         </>
     )
 }
