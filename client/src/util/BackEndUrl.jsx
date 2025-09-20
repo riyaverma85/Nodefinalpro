5fbbe30386e144207a080e104cf6abe3adf5e78a
riyaverma85 <riyaverma2976@gmail.com>
2025-09-19 17:58:40 -0700

    backend

--- client/src/util/BackEndUrl.jsx
+++ client/src/util/BackEndUrl.jsx
@@ -1,6 +1,6 @@
 
 //https://doctorpro-ndtm.onrender.com
-//const BackendUrl="http://localhost:8000";
+const BackendUrl="http://localhost:8000";
 
-const BackendUrl="https://doctorpro-ndtm.onrender.com";
+//const BackendUrl="https://doctorpro-ndtm.onrender.com";
 export default BackendUrl;
